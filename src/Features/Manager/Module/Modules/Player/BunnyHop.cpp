1d30af653057b19e98cf3affdfe043c616482cad
crave6948 <59335159+crave6948@users.noreply.github.com>
2023-12-31 17:44:51 +0700

    Refactor BunnyHop logic for better delayed jump timing

--- src/Features/Manager/Module/Modules/Player/BunnyHop.cpp
+++ src/Features/Manager/Module/Modules/Player/BunnyHop.cpp
@@ -1,10 +1,32 @@
 #include "BunnyHop.h"
+#include <random>
 namespace F {
 	namespace BunnyHopModule {
+		int generateRandomNumber(int min, int max) {
+			std::random_device rd;
+
+			std::mt19937 gen(rd());
+			std::uniform_int_distribution<int> distribution(min, max);
+
+			int random_number = distribution(gen);
+
+			return random_number;
+		}
+		inline int delayedTicks = 13;
 		void BunnyHop::onPreCreateMove(CUserCmd* cmd, C_TerrorWeapon* pWeapon, C_TerrorPlayer* pLocal)
 		{
-			if ((cmd->buttons & IN_JUMP) && !(pLocal->m_fFlags() & FL_ONGROUND))
+			if (cmd->buttons & IN_JUMP) {
+				if (!(pLocal->m_fFlags() & FL_ONGROUND)) {
 					cmd->buttons &= ~IN_JUMP;
+				}else {
+					if (delayedTicks > 0) {
+						cmd->buttons &= ~IN_JUMP;
+						delayedTicks--;
+					}else {
+						delayedTicks = generateRandomNumber(13,20);
+					}
+				}
+			}
 		}
 
 		void BunnyHop::onRender2D()
