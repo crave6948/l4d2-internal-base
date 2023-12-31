e0e2b479349a1c1636cb8ac831e59672b47f78a6
crave6948 <59335159+crave6948@users.noreply.github.com>
2023-12-31 14:51:03 +0700

    brabrabar

--- src/Features/Manager/RotationManager.cpp
+++ src/Features/Manager/RotationManager.cpp
@@ -3,22 +3,27 @@
 namespace Helper
 {
 	inline Vector lastRotation = Vector();
-	inline float getAngleDifference(float a, float b)
-	{
-		return (
-				   std::fmod(
-					   std::fmod(a - b, 360.f) + 540.f, 360.f)) -
-			   180.f;
-	}
 	inline Vector GetLocalViewAngles()
 	{
 		Vector viewAngles;
 		I::EngineClient->GetViewAngles(viewAngles);
 		return viewAngles;
 	}
+	float easeInOutQuint(float t)
+	{
+		if (t < 0.5)
+		{
+			return 16 * pow(t, 5);
+		}
+		else
+		{
+			return 1 + 16 * pow(t - 1, 5);
+		}
+	}
 	void RotationManager::onUpdate(C_TerrorPlayer *pLocal)
 	{
-		if (!I::EngineClient->IsInGame()) {
+		if (!I::EngineClient->IsInGame())
+		{
 			currentPosition = Vector();
 			targetPosition = Vector();
 			currentRotation = Vector();
@@ -31,16 +36,27 @@ namespace Helper
 		{
 			Vector vec = U::Math.AngleVectors(GetLocalViewAngles());
 			Vector vViewAngleOnWorld = pLocal->Weapon_ShootPosition() + (vec * 2.0f);
+			if (!targetPosition.IsZero()) {
+				Vector vtest = pLocal->Weapon_ShootPosition() + (vec * 1400.0f);
+				float v1 = distance(vtest, pLocal->Weapon_ShootPosition());
+				float v2 = distance(vtest, targetPosition);
+				if (v2 > v1) {
+					vViewAngleOnWorld = pLocal->Weapon_ShootPosition() + (vec * distance(pLocal->Weapon_ShootPosition(), targetPosition));
+				}
+			}
 			currentPosition = vViewAngleOnWorld;
 		}
-		if (DisabledRotation) { 
-			currentRotation = GetLocalViewAngles();
+		if (DisabledRotation)
+		{
 			targetPosition = Vector();
 			currentPosition = Vector();
+			currentRotation = GetLocalViewAngles();
 			lastMS = 0;
 			keepRotation = 0;
 			return;
-		}else {
+		}
+		else
+		{
 			if (isRotateBack())
 			{
 				Vector vec = U::Math.AngleVectors(GetLocalViewAngles());
@@ -59,30 +75,31 @@ namespace Helper
 		lastMS = I::GlobalVars->realtime;
 		DisabledRotation = false;
 	}
-    void RotationManager::ForceBack()
-    {
+	void RotationManager::ForceBack()
+	{
 		lastMS = keepRotation / 1000;
-    }
-    Vector RotationManager::getCurrentRotation()
-    {
+	}
+	Vector RotationManager::getCurrentRotation()
+	{
 		return currentRotation;
 	}
-    float RotationManager::distance(Vector current, Vector target)
-    {
-        float distanceValue = sqrt(pow(target.x - current.x, 2) + pow(target.y - current.y, 2) + pow(target.z - current.z, 2));
-        return distanceValue;
-    }
-    bool RotationManager::isRotateBack()
-    {
-        return I::GlobalVars->realtime - lastMS >= keepRotation / 1000;
-    }
-    bool RotationManager::ShouldDisabledRotation()
-    {
-        if (!isRotateBack()) return false;
+	float RotationManager::distance(Vector current, Vector target)
+	{
+		float distanceValue = sqrt(pow(target.x - current.x, 2) + pow(target.y - current.y, 2) + pow(target.z - current.z, 2));
+		return distanceValue;
+	}
+	bool RotationManager::isRotateBack()
+	{
+		return I::GlobalVars->realtime - lastMS >= keepRotation / 1000;
+	}
+	bool RotationManager::ShouldDisabledRotation()
+	{
+		if (!isRotateBack())
+			return false;
 		return DisabledRotation || distance(currentPosition, targetPosition) <= 1;
-    }
-    void RotationManager::calcPosition()
-    {
+	}
+	void RotationManager::calcPosition()
+	{
 		Vector diffPosition = targetPosition - currentPosition;
 		currentPosition += diffPosition / 2;
 	}
