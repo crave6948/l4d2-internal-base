b08285fbdf19fcc522b9e988ff2ac17baea6337f
crave6948 <59335159+crave6948@users.noreply.github.com>
2024-01-01 18:34:15 +0700

    Adjust realistic turn speed calculation for larger rotation differences
    Increase the threshold for recalculating realistic turn speed from 30 to
    60 to provide more accurate handling of larger rotation differences,
    improving movement behavior. Ensure realisticTurnSpeed reflects the
    actual capabilities of the entity. Issue #123.

--- src/Features/Manager/RotationManager.cpp
+++ src/Features/Manager/RotationManager.cpp
@@ -53,11 +53,11 @@ namespace Helper {
 		double nextGassain = distribution(generator);
 		float supposedTurnSpeed = nextGassain;
 		float realisticTurnSpeed = round(rotationDiff * (supposedTurnSpeed / 180.0f));
-		if (rotationDiff > 30)
+		if (rotationDiff > 60)
 		{
 			float a1 = (-cos(rotationDiff / 180.f * M_PI) * 0.5f + 0.5f);
 			float a2 = (1.f - (-cos(rotationDiff / 180.f * M_PI) * 0.5f + 0.5f));
-			realisticTurnSpeed = static_cast<float>(pow(a1, 2.0)) * nextGassain + static_cast<float>(pow(a2, 2.0)) * 10;
+			realisticTurnSpeed = round(pow(a1, 2.0f) * 30 + pow(a2, 2.0f) * 10);
 		}
 		if (diffRotation.x > realisticTurnSpeed)
 		{
