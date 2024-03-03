13128c052f686f62764d740ba909d201d76922c4
crave6948 <59335159+crave6948@users.noreply.github.com>
2024-03-03 17:30:36 +0700

    Test File System #1 Test saving file

--- src/Client/FileManager/FileManager.cpp
+++ src/Client/FileManager/FileManager.cpp
@@ -7,5 +7,49 @@ namespace Client::File
 
     void FileManager::init()
     {
+        save();
+    }
+    void FileManager::save()
+    {
+        std::filesystem::path currentPath = std::filesystem::current_path();
+        // Create a JSON object
+        nlohmann::json data;
+        data["name"] = "John";
+        data["age"] = 30;
+        data["city"] = "New York";
+
+        // Create an array and add some elements to it
+        nlohmann::json hobbies = {"reading", "hiking", "coding"};
+        data["hobbies"] = hobbies;
+
+        //file path
+        std::string filePath = currentPath.string() + "/output.json";
+        // Open the output file
+        std::ofstream file(filePath);
+        //check if file exists
+        if (std::filesystem::exists(filePath))
+        {
+            std::cout << "File exists" << std::endl;
+        }
+        else
+        {
+            file = std::ofstream(filePath, std::ofstream::out);
+            std::cout << "File does not exist" << std::endl;
+        }
+        write_new_file(file, data, true);
+    }
+    void FileManager::write_new_file(std::ofstream& file, nlohmann::json data, bool overwrite)
+    {
+        // Check if the file is opened successfully
+        if (!file.is_open())
+        {
+            return;
+        }
+
+        // Write JSON data to the file
+        file << std::setw(4) << data << std::endl;
+
+        // Close the file
+        file.close();
     }
 }
--- src/Client/FileManager/FileManager.h
+++ src/Client/FileManager/FileManager.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include "../../Util/Json/json.hpp"
 namespace Client::File
 {
     class FileManager
@@ -8,5 +12,7 @@ namespace Client::File
     public:
         FileManager();
         void init();
+        void save();
+        void write_new_file(std::ofstream& file, nlohmann::json data, bool overwrite);
     };
 }
